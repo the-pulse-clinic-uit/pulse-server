3cc420187ca3efd07206a80b7064dc2fadbd26ea
Khong <hankhongg@gmail.com>
2025-10-07 00:50:08 +0700

    init

---
+++ src/test/java/com/pulseclinic/pulse_server/PulseClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulseclinic.pulse_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
