6cabdd1d2b5db5889ff66f9cae4992751089047b
Khong <hankhongg@gmail.com>
2026-01-13 14:58:18 +0700

    ci: separate build n test jobs

--- src/test/java/com/pulseclinic/pulse_server/PulseClinicApiApplicationTests.java
+++ src/test/java/com/pulseclinic/pulse_server/PulseClinicApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.pulseclinic.pulse_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PulseClinicApiApplicationTests {
 
 	@Test
