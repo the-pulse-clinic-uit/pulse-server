76471d6a39fc482a42fd30145a5a9f49fe54aa94
Khong <hankhongg@gmail.com>
2026-01-13 16:13:13 +0700

    feat(deploy): set up Dockerfile

--- src/main/java/com/pulseclinic/pulse_server/PulseClinicApiApplication.java
+++ src/main/java/com/pulseclinic/pulse_server/PulseClinicApiApplication.java
@@ -4,10 +4,12 @@ import com.pulseclinic.pulse_server.security.config.OtpProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.TimeZone;
 
 @EnableConfigurationProperties(OtpProperties.class)
+@EnableJpaRepositories(basePackages = "com.pulseclinic.pulse_server")
 @SpringBootApplication
 public class PulseClinicApiApplication {
 
